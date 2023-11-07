f318f86cad071accee6f95c6ce69cbbff7f9d1dd
Becca Rothwell <becca.rothwell@gmail.com>
2023-11-07 20:39:52 +0000

    feat: Add parseRoverInput to validate rover placement instructions

--- src/ui/parse_input.ts
+++ src/ui/parse_input.ts
@@ -1,4 +1,5 @@
 import { PlateauCoordinates } from "../plateau/plateau.types";
+import { DIRECTIONS, Rover, RoverDirection } from "../rover/rover.types";
 
 export function parsePlateauInput(
   input: string
@@ -14,4 +15,26 @@ export function parsePlateauInput(
   }
 
   return [x, y];
+}
+
+export function parseRoverInput(input: string): Rover | undefined {
+  if (input.length > 5 || input.charAt(1) !== " " || input.charAt(3) !== " ") {
+    return undefined;
+  }
+  const x = Number(input.charAt(0));
+  const y = Number(input.charAt(2));
+  const direction = input.charAt(4);
+
+  if (isNaN(x) || isNaN(y)) {
+    return undefined;
+  }
+  if (!isRoverDirection(direction)) {
+    return undefined;
+  }
+
+  return { x: x, y: y, direction: direction };
+}
+
+function isRoverDirection(input: string): input is RoverDirection {
+  return DIRECTIONS.filter((direction) => direction === input).length > 0;
 }
