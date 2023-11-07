ac5ca6be6dc9dc856555bb23315d02d8ec5c61e2
Becca Rothwell <becca.rothwell@gmail.com>
2023-11-07 19:18:35 +0000

    feat: Update definePlateau to take an input string as argument and use parsePlateauInput to parse coordinates

--- src/plateau/plateau.ts
+++ src/plateau/plateau.ts
@@ -1,6 +1,17 @@
-export function definePlateau(x: number, y: number): Plateau {
+import { parsePlateauInput } from "../ui/parse_input";
+import { PlateauCoordinates, Plateau } from "./plateau.types";
+
+export function definePlateau(input: string): Plateau {
+  const coordinates = parsePlateauInput(input);
+
+  if (coordinates === undefined) {
+    throw new Error(
+      "Invalid plateau coordinates. Coordinates must be two numbers seperated by a space, e.g. '5 5'"
+    );
+  }
+
   return {
-    x: x,
-    y: y,
+    x: coordinates[0],
+    y: coordinates[1],
   };
 }
