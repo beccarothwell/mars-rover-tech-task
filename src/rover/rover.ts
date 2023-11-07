92f971a3c266496d0157554e7de43b4288b9d23c
Becca Rothwell <becca.rothwell@gmail.com>
2023-11-07 21:31:44 +0000

    feat: Update placeRover to take plateau as argument as well as string input, and throw errors if input coordinates are outside the plateau coordinates

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -1,7 +1,8 @@
 import { Rover } from "./rover.types";
 import { parseRoverInput } from "../ui/parse_input";
+import { Plateau } from "../plateau/plateau.types";
 
-export function placeRover(input: string): Rover {
+export function placeRover(plateau: Plateau, input: string): Rover {
   const roverPlacement = parseRoverInput(input);
 
   if (roverPlacement === undefined) {
@@ -9,5 +10,24 @@ export function placeRover(input: string): Rover {
       "Invalid Rover placement instructions. Instructions must be two numbers, x and y coordinates, seperated by a space, followed by a direction seperated by a space e.g. '1 2 N'"
     );
   }
-  return roverPlacement;
+
+  const roverCoordinates = roverPlacement[0];
+  const roverDirection = roverPlacement[1];
+
+  if (roverCoordinates[0] > plateau.x) {
+    throw new Error(
+      `Rover cannot be placed outside of the Plateau maximum x coordinate is ${plateau.x}`
+    );
+  }
+  if (roverCoordinates[1] > plateau.y) {
+    throw new Error(
+      `Rover cannot be placed outside of the Plateau maximum y coordinate is ${plateau.y}`
+    );
+  }
+
+  return {
+    x: roverCoordinates[0],
+    y: roverCoordinates[1],
+    direction: roverDirection,
+  };
 }
