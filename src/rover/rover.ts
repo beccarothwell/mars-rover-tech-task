074bb6f1ee4bb512021f304d0601e68c5b10d3d5
Becca Rothwell <becca.rothwell@gmail.com>
2023-11-08 17:58:56 +0000

    feat: Add moveRover to take input instructions and return rover with updated coordinates and direction

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -1,5 +1,10 @@
-import { Rover } from "./rover.types";
-import { parseRoverInput } from "../ui/parse_input";
+import {
+  Rover,
+  DIRECTIONS,
+  RoverDirection,
+  RoverMovementInstruction,
+} from "./rover.types";
+import { parseRoverInput, parseMovementInput } from "../ui/parse_input";
 import { Plateau } from "../plateau/plateau.types";
 
 export function placeRover(plateau: Plateau, input: string): Rover {
@@ -30,4 +35,71 @@ export function placeRover(plateau: Plateau, input: string): Rover {
     y: roverCoordinates[1],
     direction: roverDirection,
   };
+}
+
+export function moveRover(rover: Rover, input: string) {
+  const instructions = input.split("");
+  let currentDirection = rover.direction;
+  let currentXCoordinate = rover.x;
+  let currentYCoordinate = rover.y;
+
+  instructions.forEach((instruction) => {
+    const parseInst = parseMovementInput(instruction);
+    if (parseInst === undefined) {
+      throw new Error(
+        "Invalid movement instruction. Valid instructions are 'L', 'R', or 'M'"
+      );
+    }
+    if (parseInst === "L") {
+      switch (currentDirection) {
+        case "N":
+          currentDirection = "W";
+          break;
+        case "E":
+          currentDirection = "N";
+          break;
+        case "S":
+          currentDirection = "E";
+          break;
+        case "W":
+          currentDirection = "S";
+          break;
+      }
+    } else if (parseInst === "R") {
+      switch (currentDirection) {
+        case "N":
+          currentDirection = "E";
+          break;
+        case "E":
+          currentDirection = "S";
+          break;
+        case "S":
+          currentDirection = "W";
+          break;
+        case "W":
+          currentDirection = "N";
+          break;
+      }
+    } else if (parseInst === "M") {
+      switch (currentDirection) {
+        case "N":
+          currentXCoordinate++;
+          break;
+        case "E":
+          currentYCoordinate++;
+          break;
+        case "S":
+          currentXCoordinate--;
+          break;
+        case "W":
+          currentYCoordinate--;
+          break;
+      }
+    }
+  });
+
+  rover.x = currentXCoordinate;
+  rover.y = currentYCoordinate;
+  rover.direction = currentDirection;
+  return rover;
 }
