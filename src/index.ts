c2dd73f0ca1beea07357801fb484780b5f6c3be9
Becca Rothwell <becca.rothwell@gmail.com>
2023-11-09 01:54:47 +0000

    feat: Add inputs and movement instructions to create a Plateau, two Rovers, and move each Rover

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export function rover() {}
+import { createPlateau } from "./plateau/plateau";
+import { moveRover, createRover, roverStatus } from "./rover/rover";
+
+const plateauInput = "5 5";
+const rover1Input = "1 2 N";
+const rover1Instructions = "LMLMLMLMM";
+const rover2Input = "3 3 E";
+const rover2Instructions = "MMRMMRMRRM";
+
+const plateau = createPlateau(plateauInput);
+const rover1 = createRover(plateau, rover1Input);
+moveRover(plateau, rover1, rover1Instructions);
+console.log(roverStatus(rover1));
+
+const rover2 = createRover(plateau, rover2Input);
+moveRover(plateau, rover2, rover2Instructions);
+console.log(roverStatus(rover2));
